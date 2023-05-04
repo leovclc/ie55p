882b81d3f47ad130dcf200eeef7ed51b84c58315
leovclc <leozhao159@gmail.com>
2023-05-05 03:52:29 +1000

    scroll push

--- src/NavigationBar/NavigationBar.js
+++ src/NavigationBar/NavigationBar.js
@@ -45,33 +45,33 @@ function NavigationBar() {
         el.addEventListener('scroll', listener)
       }
   
-      const scrollto = (el) => {
-        let header = select('#header')
-        let offset = header.offsetHeight
-  
-        if (!header.classList.contains('header-scrolled')) {
-          offset -= 20
+    const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+        offset -= 20
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+        top: elementPos - offset,
+        behavior: 'smooth'
+    })
+    }
+
+    let selectHeader = select('#header')
+    if (selectHeader) {
+    const headerScrolled = () => {
+        if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+        } else {
+        selectHeader.classList.remove('header-scrolled')
         }
-  
-        let elementPos = select(el).offsetTop
-        window.scrollTo({
-          top: elementPos - offset,
-          behavior: 'smooth'
-        })
-      }
-  
-      let selectHeader = select('#header')
-      if (selectHeader) {
-        const headerScrolled = () => {
-          if (window.scrollY > 100) {
-            selectHeader.classList.add('header-scrolled')
-          } else {
-            selectHeader.classList.remove('header-scrolled')
-          }
-        }
-        window.addEventListener('load', headerScrolled)
-        onscroll(document, headerScrolled)
-      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+    }
 
     return(
         <header
